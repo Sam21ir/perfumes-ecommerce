a30dafd55b23743dcf2157c89470245adbd43a9a
SAMIR EL ALAMI <elalamisamirr@gmail.com>
2025-12-17 15:20:58 +0100

    navbar v1

--- src/App/store.js
+++ src/App/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../slices/cartSlice';
-import wishlistReducer from '../slices/wishlistSlice';
+import cartReducer from '../Slices/cartSlice';
+import wishlistReducer from '../Slices/wishListSlice';
 
 const store = configureStore({
   reducer: {
